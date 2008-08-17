397c6764ded68e394b4c30195b799c32e8ee63d1
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2008-08-17 19:34:56 +0200

    Brough Manifest.txt up to date Moved wsdl files into vendor/ since they break any rdoc attempts

--- config/hoe.rb
+++ config/hoe.rb
@@ -40,7 +40,9 @@ RDOC_OPTS = ['--quiet', '--title', 'postini documentation',
     "--opname", "index.html",
     "--line-numbers",
     "--main", "README",
-    "--inline-source"]
+    "--inline-source",
+    "--exclude", "*.wsdl"
+    ]
 
 class Hoe
   def extra_deps
@@ -64,8 +66,8 @@ $hoe = Hoe.new(GEM_NAME, VERS) do |p|
   p.changes = p.paragraphs_of("History.txt", 0..1).join("\n\n")
   #p.extra_deps = EXTRA_DEPENDENCIES
 
-    #p.spec_extras = {}    # A hash of extra values to set in the gemspec.
-  end
+  #p.spec_extras = {}    # A hash of extra values to set in the gemspec.
+end
 
 CHANGES = $hoe.paragraphs_of('History.txt', 0..1).join("\\n\\n")
 PATH    = (RUBYFORGE_PROJECT == GEM_NAME) ? RUBYFORGE_PROJECT : "#{RUBYFORGE_PROJECT}/#{GEM_NAME}"
---
+++ lib/postini/api.rb
@@ -0,0 +1,9 @@
+# See #Postini::API
+module Postini
+  
+  # This module contains all the wsdl2ruby uglies and is not documented, since
+  # this gem is supposed to abstract it away
+  module API #:nodoc: all
+      
+  end
+end
