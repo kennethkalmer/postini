a968100ed8000d486733bcf40ba557fa899a1a97
Ranaldo Bowker <ranaldo.bowker@gmail.com>
2014-02-21 03:46:00 +0200

    Do not change the LOAD_PATH, RubyGems does this for us. Do not require RubyGems, its part of Ruby 1.9.2

--- lib/postini.rb
+++ lib/postini.rb
@@ -1,8 +1,4 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
 # requirements
-require 'rubygems'
 require 'curb'
 require 'nokogiri'
 require 'handsoap'
